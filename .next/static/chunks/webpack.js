99017908fd41e104ce9583acf3ad1aad723bda82
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2024-12-02 15:07:33 +0000

    fix: 10

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1055d96f405ca6cd"; }
+/******/ 		__webpack_require__.h = function() { return "5b4793b8293e603f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
